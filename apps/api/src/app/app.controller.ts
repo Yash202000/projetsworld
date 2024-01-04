51c709f1e7faf9a97c4b716fa358ec204d466f70
MandavkarPranjal <pranjalmandavkar10@gmail.com>
2024-01-04 21:37:59 +0530

    Added api functionality and sign-up page

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Delete, Param} from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -6,8 +6,27 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get('/all-users')
+  getUsers() {
+    return this.appService.getAllUser();
+  }
+
+  @Post('/create-user')
+  createUser(@Body() data : {email : string, password : string}) {
+    console.log(data);
+    return this.appService.createUser(data);
+  }
+
+  @Patch('/patchUser')
+  patchUser(@Body() data : {email : string, password : string}) {
+    console.log(data);
+    return this.appService.patchUser(data);
+  }
+
+  @Delete("deleteUser/:id")
+  deleteUser(@Param('id') id : number) {
+    console.log("inside the delete user method id", id);
+    
+    return this.appService.deleteUser(+id);
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+class UserDTO {
+  id: number;
+  email: string;
+  password: string;
+}
 
 @Injectable()
 export class AppService {
+
+  // get all users from users table
+  async getAllUser() {
+    const users : UserDTO[] = await prisma.user.findMany(); // select * from users
+    return users;
+  }
+
+  async createUser(data : {email : string, password : string}) {
+    console.log("inside the create user method data", data);
+
+    // check if the user already exists
+    const tempUser = await prisma.user.findFirst({
+      where : {
+        email : data.email
+      }
+    })
+    if (tempUser) {
+      throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
+    }
+    
+    // here I am creating user..
+    const user = await prisma.user.create({
+      data : data
+    })
+
+    
+
+    return user;
+  }
+
+  async patchUser(data : {email : string, password : string}) {
+    console.log("inside the create user method data", data);
+
+    // check if the user already exists
+    const tempUser = await prisma.user.findFirst({
+      where : {
+        email : data.email
+      }
+    })
+    if (tempUser) {
+      const updatedUser = await prisma.user.update({
+        where : {
+          email : data.email
+        },
+        data : {
+          password : data.password
+        }
+      })
+
+      return updatedUser;
+    }
+    
+    throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
+
+  }
+
+  async deleteUser(id : number) {
+    const user = await prisma.user.findFirst({
+      where : {
+        id : id 
+      }
+    })
+
+    if(!user) throw new HttpException("User not found", HttpStatus.BAD_REQUEST);
+
+    const deletedUser = await prisma.user.delete({
+      where : {
+        id : id
+      }
+    })
+
+    return deletedUser;
+  }
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
+
 }
