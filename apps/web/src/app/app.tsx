f6ff50646d4717d62f6745c90db6212bba6f0b01
mestrymithil <mestrymithil@gmail.com>
2024-01-04 10:05:38 +0530

    updated

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,14 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="Mithil" />
+      <NxWelcome title="Mithil mestry" />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
