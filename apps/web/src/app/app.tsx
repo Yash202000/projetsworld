ed7653f700f4748ca81a35560798f781daaae5e2
OmShinde-23 <omwindows2003@gmail.com>
2024-01-09 15:03:51 +0530

    projetsworld installed

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,14 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import SignInSide from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="Prathmesh" />
+      <SignInSide />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
